3399de6eb670ed6db0eb71e7432daa75362b6135
Thomas Vondracek <thomas.vondracek@hotmail.com>
2022-09-20 14:37:34 +0200

    Push

--- src/Bienvenue.java
+++ src/Bienvenue.java
@@ -5,5 +5,9 @@ public class Bienvenue {
         System.out.println("Bienvenue");
 
         var scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        System.out.println("Vous avez saisi: " + input);
     }
 }
