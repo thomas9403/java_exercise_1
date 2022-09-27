8393d9419698c40a05789023ae095e0b9076e016
Thomas Vondracek <thomas.vondracek@hotmail.com>
2022-09-27 11:29:36 +0200

    push2

--- src/Launcher.java
+++ src/Launcher.java
@@ -11,35 +11,104 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 
-public class Launcher  
-{
-    public static void main(String[] args) throws IOException 
-    {
+// public class Launcher  
+// {
+//     public static void main(String[] args) throws IOException 
+//     {
         
-        System.out.println("Bienvenue !");
-        var scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        List<Command> commands = new ArrayList<Command>();
-        commands.add(new Freq());
-        commands.add(new Fibo());
+//         System.out.println("Bienvenue !");
+//         var scanner = new Scanner(System.in);
+//         String input = scanner.nextLine();
+//         List<Command> commands = new ArrayList<Command>();
+//         commands.add(new Freq());
+//         commands.add(new Fibo());
+//         commands.add(new Quit());
+//         while (!"quit".equalsIgnoreCase(input)) 
+//         {
+//             Boolean found = false;
+//             for (Command command : commands) {
+//                 if (command.name().equalsIgnoreCase(input)) 
+//                 {
+//                     found = true;
+//                     command.run(scanner);
+//                     break;
+//                 }
+//             }
+//             if (!found) 
+//             {
+//                 System.out.println("Unknown command");
+//             }
+//             input = scanner.nextLine();
+//         }
+//     }
+//     public static int fibo(int nb) 
+//     {
+//         if(nb <= 1) 
+//         {
+//             return nb;
+//         }
+//         return fibo(nb-1) + fibo(nb-2);
+//     }
+
+//     public static void freq(String path) throws IOException 
+//     {
+//         Path filePath = Paths.get(path);
+//         String content = Files.readString(filePath);
+//         if(content.isEmpty()) 
+//         {
+//             System.out.println("Empty file");
+//         }
+//         content = content.toLowerCase();
+//         Stream<String> stream = Arrays.stream(content.replaceAll("[^a-zA-Z]", " ").split(" ")).filter(word -> !word.isEmpty());
+//         Map<String,Long> result = stream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//         result.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).limit(3).forEach(entry -> System.out.print(entry.getKey() + " "));
+//         System.out.print("\n");
+//     }
+
+    
+// }
+
+
+
+public class Launcher
+{
+    public static void main(String[] args)
+    {
+        List <Command> commands = new ArrayList <>();
+        Scanner scanner = new Scanner(System.in);
         commands.add(new Quit());
-        while (!"quit".equalsIgnoreCase(input)) 
+        commands.add(new Fibo());
+        commands.add(new Freq());
+        // commands.add(new Preditct());
+        System.out.println("Bienvenue !");
+        
+        
+        while(true)
         {
-            Boolean found = false;
-            for (Command command : commands) {
-                if (command.name().equalsIgnoreCase(input)) 
+            String input = scanner.nextLine();
+            boolean result = false, check = true;
+            for(Command command : commands)
+            {
+                if(command.name().equalsIgnoreCase(input))
                 {
-                    found = true;
-                    command.run(scanner);
+                    result = command.run(scanner);
+                    check = false;
                     break;
                 }
             }
-            if (!found) 
+
+            if(result)
+            {
+                break;
+            }
+
+            if(check)
             {
                 System.out.println("Unknown command");
             }
-            input = scanner.nextLine();
         }
+        
+
     }
     public static int fibo(int nb) 
     {
@@ -64,9 +133,9 @@ public class Launcher
         result.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).limit(3).forEach(entry -> System.out.print(entry.getKey() + " "));
         System.out.print("\n");
     }
-
-    
 }
+
+
 
 interface Command 
 {
@@ -93,7 +162,6 @@ class Fibo implements Command
     public Boolean run(Scanner scanner) 
     {
         System.out.println("Entrez un nombre n :");
-        // get the input from the user and convert it to an integer using Integer.parseInt() method
         int nb = Integer.parseInt(scanner.nextLine());
         if (nb < 0) 
         {
@@ -107,7 +175,7 @@ class Fibo implements Command
     }
 }
 
-class Freq implements Command 
+class Freq implements Command
 {
     public String name() 
     {
